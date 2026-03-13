aff906a56f810cbbfbca63272316983ba5d56e5f
d11416216-bit <d11416216@gs.takming.edu.tw>
2026-03-13 12:08:13 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Class_0313_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe, mary;
+
+            joe = new Test();
+            mary = new Test();
+
+            joe.SetGrade(70, 80);
+            mary.SetGrade(70, 90);
+
+            Console.WriteLine(joe.GetAvg());
+
+            Console.WriteLine(joe.GetAvg());
+
+
+        }
+    }
+}
